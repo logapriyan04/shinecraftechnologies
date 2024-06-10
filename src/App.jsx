0f59d0fe337479638bdaafd291363e463737fdf6
logapriyan04 <logapriyanlogu6@gmail.com>
2024-06-11 03:33:34 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+
+import Home from "./Components/Home";
+
 import About from "./Components/About";
+import Header from "./Components/Header";
+import "./Styles/Style.css";
+import { RxHome } from "react-icons/rx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +16,8 @@ function App() {
   return (
     <>
       <div>
+        <Header />
+        <Home />
         <About />
       </div>
     </>
