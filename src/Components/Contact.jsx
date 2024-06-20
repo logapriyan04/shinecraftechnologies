79c02db3e1f8cabcff7239d5bb72454afdb6279d
logapriyan04 <logapriyanlogu6@gmail.com>
2024-06-20 23:24:25 +0530

    style

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,13 +1,53 @@
 import React, { useState } from "react";
 import { BsArrowUpRight } from "react-icons/bs";
-
 import { HiMiniArrowLongUp } from "react-icons/hi2";
 import { HiOutlineArrowLongDown } from "react-icons/hi2";
+import emailjs from "@emailjs/browser";
+
 function Contact() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isSuccessPopupOpen, setIsSuccessPopupOpen] = useState(false);
+  const [isErrorPopupOpen, setIsErrorPopupOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const togglePopup = () => {
     setIsPopupOpen(!isPopupOpen);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsPopupOpen(false); // Close the form popup
+
+    if (name === "" || email === "" || message === "") {
+      setIsErrorPopupOpen(true); // Show error popup
+      return;
+    }
+
+    const serviceId = "service_sav2i1r";
+    const templateId = "template_9zot3co";
+    const publicKey = "xNe_IKaCy3kcvYGQO";
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "logapriyanlogu",
+      message: message,
+    };
+
+    emailjs
+      .send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log("Email sent successfully", response);
+        setName("");
+        setEmail("");
+        setMessage("");
+        setIsSuccessPopupOpen(true); // Show success popup
+      })
+      .catch((error) => {
+        console.log("Error sending mail", error);
+      });
   };
 
   return (
@@ -15,7 +55,7 @@ function Contact() {
       <h3>HAVE YOU PROJECT IN MIND</h3>
       <div className="contact-heading">
         <h1>
-          LET'S MAKE SOMETHING <br></br> <span>GREAT TOGETHER !</span>
+          LET'S MAKE SOMETHING <br /> <span>GREAT TOGETHER !</span>
         </h1>
       </div>
       <button className="circular-button" onClick={togglePopup}>
@@ -30,38 +70,80 @@ function Contact() {
               &times;
             </span>
             <h2>CONTACT FORM</h2>
-            <form id="registerForm">
-              <div class="form-group">
+            <form
+              onSubmit={handleSubmit}
+              className="emailForm"
+              id="registerForm"
+            >
+              <div className="form-group">
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   id="name"
                   placeholder="Name"
-                ></input>
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <input
                   type="email"
-                  class="form-control"
+                  className="form-control"
                   id="email"
                   placeholder="Email"
-                ></input>
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <textarea
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   id="message"
                   placeholder="Message"
                   rows="4"
-                ></textarea>
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
               </div>
-
               <button type="submit">Submit</button>
             </form>
           </div>
         </div>
       )}
+
+      {isSuccessPopupOpen && (
+        <div id="successPopupContainer" className="successPopupContainer">
+          <div className="successPopupContent">
+            <span
+              className="closeBtn"
+              onClick={() => setIsSuccessPopupOpen(false)}
+            >
+              &times;
+            </span>
+            <h2>Thank you!</h2>
+            <p>Your message has been sent successfully.</p>
+            <button onClick={() => setIsSuccessPopupOpen(false)}>Close</button>
+          </div>
+        </div>
+      )}
+
+      {isErrorPopupOpen && (
+        <div id="errorPopupContainer" className="errorPopupContainer">
+          <div className="errorPopupContent">
+            <span
+              className="closeBtn"
+              onClick={() => setIsErrorPopupOpen(false)}
+            >
+              &times;
+            </span>
+            <h2>Error</h2>
+            <p>All fields are required.</p>
+            <button onClick={() => setIsErrorPopupOpen(false)}>Close</button>
+          </div>
+        </div>
+      )}
+
       <div>
         <a href="#Home">
           <button className="vertical-line-container-2">
